ca10be510be2740041509cad5cc87381b9ebf1e7
Tomasz Kowalczyk <tomek@kownet.info>
2012-06-27 08:21:01 +0200

    app logic added

---
+++ KinectSimpleApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectSimpleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor _sensor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                _sensor = KinectSensor.KinectSensors[0];
+
+                if (_sensor.Status == KinectStatus.Connected)
+                {
+                    _sensor.ColorStream.Enable();
+                    _sensor.DepthStream.Enable();
+                    _sensor.SkeletonStream.Enable();
+                    _sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(_sensor_AllFramesReady);
+                    _sensor.Start();
+
+                    tbStatus.Text = "Kinect Connected";
+                }
+            }
+            else
+            {
+                tbStatus.Text = "No Kinect";
+            }
+        }
+
+        private void _sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            
+        }
+        void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_sensor != null)
+            {
+                _sensor.Stop();
+            }
+        }
+    }
+}
